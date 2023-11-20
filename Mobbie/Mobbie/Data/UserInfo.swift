e8b920f83344f18371aab83e39633eb70500398a
yeoni <ghj0308@daum.net>
2023-11-21 01:55:32 +0900

    feat: UserInfo

---
+++ Mobbie/Mobbie/Data/UserInfo.swift
@@ -0,0 +1,18 @@
+//
+//  UserInfo.swift
+//  Mobbie
+//
+//  Created by yeoni on 2023/11/20.
+//
+
+import Foundation
+
+struct UserInfo {
+    var id: String
+    var password: String
+    var phoneNumber: String
+    
+    mutating func updateValue(type: JoinType) {
+        
+    }
+}
