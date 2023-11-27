f2265419adadeece404525f67ed096a444241178
yeoni <ghj0308@daum.net>
2023-11-27 23:10:20 +0900

    edit: password 정규식 변경

--- Mobbie/Mobbie/Data/RegexType.swift
+++ Mobbie/Mobbie/Data/RegexType.swift
@@ -10,6 +10,6 @@ import Foundation
 
 enum RegexType: String {
     case email = #"^([a-zA-Z0-9._-])+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,20}$"#
-    case password = #"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{6,}$"#
+    case password = #"^(?=.*[a-zA-Z])(?=.*\d).{6,}$"#
     case phoneNumber = #"[0-9]{3}\-[0-9]{4}\-[0-9]{4}"#
 }
