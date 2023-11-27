57e2560103cbd9bf2f3bfa05cb58a32906428b8a
yeoni <ghj0308@daum.net>
2023-11-28 02:46:57 +0900

    feat: WelcomeViewController

---
+++ Mobbie/Mobbie/Onboarding/Welcome/WelcomeViewController.swift
@@ -0,0 +1,109 @@
+//
+//  WelcomeViewController.swift
+//  Mobbie
+//
+//  Created by yeoni on 2023/11/28.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class WelcomeViewController: BaseViewController {
+    
+    let informationLabel = {
+        let label = UILabel()
+        label.text = "환영합니다!"
+        label.font = .systemFont(ofSize: 28, weight: .semibold)
+        return label
+    }()
+    
+    let descriptionLabel = {
+        let label = UILabel()
+        label.text = "Mobbie와 함께\n 즐거운 시간 보내세요!"
+        label.font = .systemFont(ofSize: 16, weight: .light)
+        label.textColor = .systemGray2
+        return label
+    }()
+    
+    let nextButton = {
+        let button = UIButton()
+        var config = UIButton.Configuration.filled()
+        config.baseBackgroundColor = .systemGreen
+        config.title = "시작하기"
+        button.configuration = config
+        return button
+    }()
+    
+    
+    let viewModel = WelcomeViewModel()
+    
+    var userInfo: UserInfo?
+    
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bind()
+    }
+    
+    override func configureHierarchy() {
+        super.configureHierarchy()
+        
+        [
+            informationLabel,
+            nextButton,
+            descriptionLabel
+        ]
+            .forEach({ view.addSubview($0) })
+        
+    }
+    
+    override func setConstraints() {
+        
+        informationLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(80)
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.height.equalTo(52)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(informationLabel.snp.bottom).offset(24)
+            make.horizontalEdges.equalTo(informationLabel)
+        }
+    }
+    
+    override func configureView() {
+        informationLabel.numberOfLines = 0
+        informationLabel.setLineSpacing(lineSpacing: 6)
+        
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.setLineSpacing(lineSpacing: 5)
+    }
+    
+    func bind() {
+        guard let userInfo else { return }
+        
+        let input = WelcomeViewModel.Input(
+            tap: nextButton.rx.tap,
+            userInfo: userInfo
+        )
+        let output = viewModel.transform(input: input)
+        
+        print("bind")
+        output.tap
+            .bind { _ in
+                let vc = FeedViewController()
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+    }
+    
+}
