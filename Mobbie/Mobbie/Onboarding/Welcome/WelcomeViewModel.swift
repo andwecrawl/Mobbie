585d876e67c41a2b074717b195a292c408bddace
yeoni <ghj0308@daum.net>
2023-11-28 02:47:04 +0900

    feat: WelcomeViewModel

---
+++ Mobbie/Mobbie/Onboarding/Welcome/WelcomeViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  WelcomeViewModel.swift
+//  Mobbie
+//
+//  Created by yeoni on 2023/11/28.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class WelcomeViewModel: ViewModel {
+    
+    let newInfo: BehaviorSubject<UserInfo> = BehaviorSubject(value: UserInfo(id: "", password: "", phoneNumber: ""))
+    var disposeBag = DisposeBag()
+    
+    struct Input {
+        let tap: ControlEvent<Void>
+        var userInfo: UserInfo
+    }
+    
+    struct Output {
+        let tap: ControlEvent<Void>
+    }
+    
+    func transform(input: Input) -> Output {
+        newInfo.onNext(input.userInfo)
+        
+        input.tap
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .flatMap {
+                print(self.newInfo)
+                return MoyaAPIManager.shared.fetchInSignProgress(.Login(email: input.userInfo.id, password: input.userInfo.password), type: LoginResponse.self) { print("error Handling: \($0)") }
+            }
+            .subscribe(with: self) { owner, response in
+                switch response {
+                case .success(let result):
+                    print("===== login Success!!: \(result.token), \(result.refreshToken)")
+                case .failure(let error):
+                    "======= error message: \(error.localizedDescription)"
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(
+            tap: input.tap
+        )
+    }
+}
