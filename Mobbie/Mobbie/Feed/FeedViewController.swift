25484d1432a5eac4546844358304ba2794c988c0
yeoni <ghj0308@daum.net>
2023-12-03 21:22:19 +0900

    feat: 기초 UI 작업

--- Mobbie/Mobbie/Feed/FeedViewController.swift
+++ Mobbie/Mobbie/Feed/FeedViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 final class FeedViewController: BaseViewController {
     
+    private let tableView = {
+        let view = UITableView(frame: .zero)
+        view.register(FeedTableViewCell.self, forCellReuseIdentifier: FeedTableViewCell.identifier)
+        return view
+    }()
+    
+    private let writeButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor.highlightMint
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.layer.cornerRadius = 30
+        button.makeShadow()
+        return button
+    }()
     
     
     override func viewDidLoad() {
@@ -19,10 +33,20 @@ final class FeedViewController: BaseViewController {
     override func configureHierarchy() {
         super.configureHierarchy()
         
+        [
+            tableView,
+            writeButton
+        ]
+            .forEach { view.addSubview($0) }
+        
     }
     
     override func setConstraints() {
-        
+        writeButton.snp.makeConstraints { make in
+            make.width.equalTo(60)
+            make.height.equalTo(writeButton.snp.width)
+            make.bottom.trailing.equalTo(view.safeAreaLayoutGuide).inset(16)
+        }
     }
     
     override func configureView() {
@@ -40,4 +64,18 @@ final class FeedViewController: BaseViewController {
     }
     
     
+}
+
+
+extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FeedTableViewCell.identifier) as? FeedTableViewCell else { return UITableViewCell() }
+        
+        
+        return cell
+    }
 }
