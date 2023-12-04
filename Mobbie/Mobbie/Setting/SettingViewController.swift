7558fd1b276f5f176c241387b0503df1c4ec26f3
yeoni <ghj0308@daum.net>
2023-12-05 02:12:58 +0900

    feat: SettingViewController

---
+++ Mobbie/Mobbie/Setting/SettingViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingViewController.swift
+//  Mobbie
+//
+//  Created by yeoni on 2023/12/05.
+//
+
+import UIKit
+
+class SettingViewController: BaseViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureHierarchy() {
+        
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+    override func configureView() {
+        
+    }
+    
+    
+    
+}
