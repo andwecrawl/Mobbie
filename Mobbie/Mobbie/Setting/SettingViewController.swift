939954ec84533d1c6b99ad117171433e64310767
yeoni <ghj0308@daum.net>
2023-12-05 19:07:39 +0900

    feat: tableView 구현

--- Mobbie/Mobbie/Setting/SettingViewController.swift
+++ Mobbie/Mobbie/Setting/SettingViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 final class SettingViewController: BaseViewController {
     
+    private lazy var tableView = {
+        let view = UITableView(frame: .zero, style: .insetGrouped)
+        view.dataSource = self
+        view.delegate = self
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,18 +22,37 @@ final class SettingViewController: BaseViewController {
     }
     
     override func configureHierarchy() {
-        
+        view.addSubview(tableView)
         
     }
     
     override func setConstraints() {
-        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
     override func configureView() {
         
     }
     
+    
+    
+}
+
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "hello"
+        return cell
+    }
+    
+    
+
     
     
 }
