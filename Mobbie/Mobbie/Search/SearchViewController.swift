d10695a6f952fdcae6c303eb919816e91e307f5e
yeoni <ghj0308@daum.net>
2023-12-05 14:31:00 +0900

    feat: SearchViewController

---
+++ Mobbie/Mobbie/Search/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  Mobbie
+//
+//  Created by yeoni on 2023/12/05.
+//
+
+import UIKit
+
+class SearchViewController: BaseViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+    override func configureView() {
+        
+    }
+    
+}
