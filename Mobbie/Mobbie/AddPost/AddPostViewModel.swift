b14ec7a1a5b1e7e1cd35eec63ead52265fe2d685
yeoni <ghj0308@daum.net>
2023-12-07 01:15:03 +0900

    feat: AddPostViewModel

---
+++ Mobbie/Mobbie/AddPost/AddPostViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  AddPostViewModel.swift
+//  Mobbie
+//
+//  Created by yeoni on 2023/12/07.
+//
+
+import UIKit
+import RxSwift
+
+class AddPostViewModel: ViewModel {
+    
+    var disposeBag = DisposeBag()
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output? {
+        
+        return Output()
+    }
+}
