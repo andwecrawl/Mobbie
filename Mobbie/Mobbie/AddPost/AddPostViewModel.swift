4812ffb56de9e7633f54915ef880b49f1dd3d6d3
yeoni <ghj0308@daum.net>
2023-12-08 00:25:32 +0900

    feat: transform 로직 추가

--- Mobbie/Mobbie/AddPost/AddPostViewModel.swift
+++ Mobbie/Mobbie/AddPost/AddPostViewModel.swift
@@ -7,21 +7,59 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class AddPostViewModel: ViewModel {
     
     var disposeBag = DisposeBag()
     
     struct Input {
-        
+        let addButtonTapped: ControlEvent<Void>
+        let text: ControlProperty<String>
     }
     
+    var transition: TransitionProtocol?
+    var alert: ((String, String) -> ())?
+    
     struct Output {
-        
+        let addButtonTapped: ControlEvent<Void>
+        let isSaved: PublishRelay<Bool>
+        let errorMessage: BehaviorRelay<String>
     }
     
     func transform(input: Input) -> Output? {
         
-        return Output()
+        let isSaved = PublishRelay<Bool>()
+        let errorMessage = BehaviorRelay(value: "")
+
+        input.addButtonTapped
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .withLatestFrom(input.text)
+            .flatMap { str in
+                MoyaAPIManager.shared.fetchInSignProgress(.writePost(model: PostModel(content: str, product_id: "")), type: Posts.self)
+            }
+            .subscribe(with: self) { owner, response in
+                switch response {
+                case .success(_):
+                    isSaved.accept(true)
+                case .failure(let error):
+                    isSaved.accept(false)
+                    errorMessage.accept(error.localizedDescription)
+                }
+            } onError: { owner, error in
+                if error.localizedDescription == "expiredRefreshToken" {
+                    owner.alert?("세션이 만료되었습니다.", "다시 로그인 해주세요!")
+                    owner.transition?.transitionTo(LoginViewController())
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        
+        
+        return Output(
+            addButtonTapped: input.addButtonTapped,
+            isSaved: isSaved,
+            errorMessage: errorMessage
+        )
     }
 }
