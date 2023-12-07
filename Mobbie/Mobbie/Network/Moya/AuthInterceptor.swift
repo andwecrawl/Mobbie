79eb4f4a454201537a7b9abe316fa8b904c56c3c
yeoni <ghj0308@daum.net>
2023-12-08 00:23:36 +0900

    bug: interceptor만 타면 앱이 꺼지는 오류 수정

--- Mobbie/Mobbie/Network/Moya/AuthInterceptor.swift
+++ Mobbie/Mobbie/Network/Moya/AuthInterceptor.swift
@@ -10,7 +10,7 @@ import RxSwift
 import Alamofire
 import Moya
 
-final class AuthInterceptor: RequestInterceptor {
+final class AuthInterceptor: RequestInterceptor, TransitionProtocol {
     
     static let shared = AuthInterceptor()
 
@@ -21,21 +21,31 @@ final class AuthInterceptor: RequestInterceptor {
     func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
         
            print("retry 진입")
-        guard let response = request.task?.response as? HTTPURLResponse, response.statusCode == 419
+        guard let response = request.task?.response as? HTTPURLResponse, response.statusCode == 419 || response.statusCode == 418
            else {
             print("error?")
-               completion(.doNotRetry)
+            completion(.doNotRetryWithError(error))
                return
            }
-
-           // statusCode == 419
-        MoyaAPIManager.shared.fetchInSignProgress(.refreshAccessToken, type: TokenResponse.self)
+        
+        if response.statusCode == 418 {
+            transitionTo(LoginViewController())
+        }
+        
+        let task = Observable.just(())
+        
+        task
+            .observe(on: MainScheduler.asyncInstance)
+            .flatMap{ MoyaAPIManager.shared.fetchInSignProgress(.refreshAccessToken, type: TokenResponse.self) }
             .subscribe(with: self) { owner, response in
                 switch response {
                 case .success(let result):
                     // 토큰 재발급 성공 -> 갈아끼우기
+                    print(UserDefaultsHelper.shared.accessToken)
+                    print(result.token)
                     UserDefaultsHelper.shared.accessToken = result.token
                     
+                    print(UserDefaultsHelper.shared.accessToken)
                     completion(.retry)
                 case .failure(let error):
                     // 갱신 실패 -> 로그인 화면으로 전환
