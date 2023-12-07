d885e4d975d5c41d5e1f3afc20448be7ec0feea0
yeoni <ghj0308@daum.net>
2023-12-08 00:25:15 +0900

    remove: 주석 삭제

--- Mobbie/Mobbie/AppDelegate.swift
+++ Mobbie/Mobbie/AppDelegate.swift
@@ -12,7 +12,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-//        UILabel.appearance().textColor = .content
         UINavigationBar.appearance().tintColor = UIColor.highlightOrange
         UITabBar.appearance().tintColor = UIColor.highlightOrange
         UIButton.appearance().tintColor = UIColor.highlightOrange
