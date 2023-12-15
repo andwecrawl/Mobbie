00bc56997ff2334477077b82d2a222b49b8acfee
yeoni <ghj0308@daum.net>
2023-12-15 23:12:55 +0900

    edit: 필요없는 객체 삭제

--- Mobbie/Mobbie/Scene/Detail/CommentTableViewCell.swift
+++ Mobbie/Mobbie/Scene/Detail/CommentTableViewCell.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-class CommentTableViewCell: BaseTableViewCell {
+protocol CommentDelegate {
+    func delete(tag: Int, postID: String, commentID: String)
+    func modifiy()
+}
+
+final class CommentTableViewCell: BaseTableViewCell {
     
     private let userLabel = {
         let label = UILabel()
@@ -24,60 +29,9 @@ class CommentTableViewCell: BaseTableViewCell {
     
     private let contentLabel = {
         let label = UILabel()
-        label.font = Design.Font.preRegular.getFonts(size: 15)
+        label.font = Design.Font.preRegular.largeFont
         label.numberOfLines = 0
         return label
-    }()
-    
-    private let commentButton = {
-        let button = UIButton()
-        let imageConfig = UIImage.SymbolConfiguration(pointSize: 13, weight: .medium)
-        let image = UIImage(systemName: "bubble.left", withConfiguration: imageConfig)
-        var config = UIButton.Configuration.plain()
-        var titleContainer = AttributeContainer()
-        titleContainer.font = Design.Font.preLight.smallFont
-        titleContainer.foregroundColor = .gray.withAlphaComponent(0.9)
-        config.attributedTitle = AttributedString("10", attributes: titleContainer)
-        config.image = image
-        config.imagePlacement = .leading
-        config.imagePadding = 4
-        button.configuration = config
-        button.tintColor = .gray.withAlphaComponent(0.8)
-        return button
-    }()
-    
-    let likedButton = {
-        let button = UIButton()
-        let imageConfig = UIImage.SymbolConfiguration(pointSize: 13, weight: .medium)
-        let heartImage = UIImage(systemName: "heart", withConfiguration: imageConfig)
-        let filledHeart = UIImage(systemName: "heart.fill", withConfiguration: imageConfig)
-        button.setImage(heartImage, for: .normal)
-        button.setImage(filledHeart, for: .selected)
-        var config = UIButton.Configuration.plain()
-        var titleContainer = AttributeContainer()
-        titleContainer.font = Design.Font.preLight.smallFont
-        titleContainer.foregroundColor = .gray.withAlphaComponent(0.9)
-        config.attributedTitle = AttributedString("10", attributes: titleContainer)
-        config.imagePlacement = .leading
-        config.imagePadding = 5
-        config.baseBackgroundColor = .clear
-        button.configuration = config
-        button.tintColor = .gray.withAlphaComponent(0.8)
-        return button
-    }()
-    
-    let shareButton = {
-        let button = UIButton()
-        let imageConfig = UIImage.SymbolConfiguration(pointSize: 13, weight: .medium)
-        let image = UIImage(systemName: "square.and.arrow.up", withConfiguration: imageConfig)
-        var config = UIButton.Configuration.plain()
-        config.image = image
-        config.imagePlacement = .leading
-        config.imagePadding = 4
-        config.baseBackgroundColor = .clear
-        button.configuration = config
-        button.tintColor = .gray.withAlphaComponent(0.8)
-        return button
     }()
     
     let settingButton = {
@@ -89,33 +43,24 @@ class CommentTableViewCell: BaseTableViewCell {
         return button
     }()
     
-    let buttonStackView = UIStackView()
-    
+    var postID: String?
     var comment: Comment?
-    
     
     override func prepareForReuse() {
         super.prepareForReuse()
         userLabel.text = ""
         timeLabel.text = "몇 시간 전"
         contentLabel.text = "내용이에용"
-        likedButton.isSelected = false
         settingButton.isHidden = false
     }
     
     
     override func configureHierarchy() {
         
-        buttonStackView.axis = .horizontal
-        buttonStackView.spacing = 8
-        buttonStackView.distribution = .equalSpacing
-        buttonStackView.AddArrangedSubviews([commentButton, likedButton, shareButton])
-        
         [
             userLabel,
             timeLabel,
             contentLabel,
-            buttonStackView,
             settingButton
         ]
             .forEach { contentView.addSubview($0) }
@@ -142,22 +87,6 @@ class CommentTableViewCell: BaseTableViewCell {
             make.horizontalEdges.equalToSuperview().inset(20)
         }
         
-        buttonStackView.spacing = 10
-        buttonStackView.distribution = .equalSpacing
-        buttonStackView.alignment = .leading
-        buttonStackView.snp.makeConstraints { make in
-            make.top.equalTo(contentLabel.snp.bottom)
-            make.leading.equalTo(contentLabel)
-            make.trailing.equalTo(contentLabel).inset(100)
-            make.height.equalTo(35)
-            make.bottom.equalToSuperview().inset(4)
-        }
-        
-        [commentButton, likedButton, shareButton].forEach { button in
-            button.snp.makeConstraints { make in
-                make.width.equalTo(button.snp.height).multipliedBy(1.25)
-            }
-        }
     }
     
     override func configureView() {
