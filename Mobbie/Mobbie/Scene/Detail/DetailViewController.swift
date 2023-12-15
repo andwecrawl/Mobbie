f78f13bacf492504ee5f97ec3e0e74a90042f0d6
yeoni <ghj0308@daum.net>
2023-12-15 23:13:19 +0900

    edit: tableView 연결

--- Mobbie/Mobbie/Scene/Detail/DetailViewController.swift
+++ Mobbie/Mobbie/Scene/Detail/DetailViewController.swift
@@ -7,19 +7,20 @@
 
 import UIKit
 
-class DetailViewController: BaseViewController {
+final class DetailViewController: BaseViewController {
     
     private lazy var tableView = {
         let view = UITableView(frame: .zero)
         view.register(FeedTableViewCell.self, forCellReuseIdentifier: FeedTableViewCell.identifier)
+        view.register(CommentTableViewCell.self, forCellReuseIdentifier: CommentTableViewCell.identifier)
         view.rowHeight = UITableView.automaticDimension
         view.estimatedRowHeight = 100
-//        view.delegate = self
-//        view.dataSource = self
+        view.delegate = self
+        view.dataSource = self
         return view
     }()
     
-    
+    var post: Posts?
     
     
     
@@ -29,14 +30,54 @@ class DetailViewController: BaseViewController {
     
 }
 
-//extension DetailViewController: UITableViewDelegate, UITableViewDataSource {
-//    
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        <#code#>
-//    }
-//    
-//}
+extension DetailViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let post else { return 0 }
+        return 1 + post.comments.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: FeedTableViewCell.identifier) as? FeedTableViewCell else { return UITableViewCell() }
+            
+            cell.post = post
+            cell.configureCell()
+            
+            return cell
+        } else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CommentTableViewCell.identifier) as? CommentTableViewCell else { return UITableViewCell() }
+            guard let post else { return UITableViewCell() }
+            let index = indexPath.row - 1
+            let comment = post.comments
+            
+            cell.postID = post._id
+            cell.comment = comment[index]
+            
+            return cell
+        }
+    }
+    
+}
+
+
+extension DetailViewController: FeedDelegate {
+    func like(tag: Int, result: Bool) {
+        
+        
+    }
+    
+    func delete(tag: Int, postID: String) {
+        
+        
+    }
+    
+    func modifiy() {
+        
+    }
+    
+    func moveComment(tag: Int) {
+        sendOneSideAlert(title: "현재 댓글창에 있어요!", message: "")
+    }
+}
+
