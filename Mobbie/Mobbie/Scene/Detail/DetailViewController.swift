46a8b21049e286d2f3d2f1817f4561e1f11e6349
yeoni <ghj0308@daum.net>
2023-12-16 23:50:18 +0900

    feat: DetailVC UI

--- Mobbie/Mobbie/Scene/Detail/DetailViewController.swift
+++ Mobbie/Mobbie/Scene/Detail/DetailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RxSwift
 
 final class DetailViewController: BaseViewController {
     
@@ -20,10 +21,52 @@ final class DetailViewController: BaseViewController {
         return view
     }()
     
-    let userInputView = CommentInputView()
+    private let addButton = {
+        let view = UIButton()
+        var config = UIButton.Configuration.filled()
+        config.cornerStyle = .capsule
+        config.baseBackgroundColor = .highlightOrange
+        config.title = "게시하기"
+        var container = AttributeContainer()
+        container.foregroundColor = UIColor.white
+        container.font = Design.Font.preMedium.midFont
+        config.attributedTitle = AttributedString("게시하기", attributes: container)
+        view.configuration = config
+        return view
+    }()
     
+    private let limitLabel = {
+        let label = UILabel()
+        label.font = Design.Font.preSemiBold.midFont
+        label.textColor = UIColor.highlightMint
+        label.text = "23123123/200"
+        label.snp.makeConstraints { make in
+            make.width.equalTo(66)
+        }
+        label.textAlignment = .center
+        return label
+    }()
+    
+    
+    let userInputView = CommentInputView()
+    let disposeBag = DisposeBag()
     var post: Posts?
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardUp), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDown), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
     
     override func configureHierarchy() {
         
@@ -39,18 +82,50 @@ final class DetailViewController: BaseViewController {
             make.edges.equalTo(view.safeAreaLayoutGuide)
         }
         
+        userInputView.sizeToFit()
         userInputView.snp.makeConstraints { make in
             make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.height.lessThanOrEqualTo(150)
+            make.height.greaterThanOrEqualTo(50)
         }
+        userInputView.backgroundColor = .green
     }
     
     
     override func configureView() {
+        let toolbar = UIToolbar()
+
+        let post = UIBarButtonItem(customView: addButton)
+        addButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+        let label = UIBarButtonItem(customView: limitLabel)
+        label.isEnabled = true
+        let flexibleSpaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let fixedSpace = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        toolbar.setItems([flexibleSpaceButton, label, fixedSpace, post], animated: false)
+        toolbar.sizeToFit()
+        userInputView.textView.inputAccessoryView = toolbar
+    }
+    
+    @objc func addButtonTapped() {
         
     }
     
+    @objc func keyboardUp(notification:NSNotification) {
+        if let keyboardFrame:NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+           let keyboardRectangle = keyboardFrame.cgRectValue
+       
+            UIView.animate(
+                withDuration: 0.3
+                , animations: {
+                    self.userInputView.transform = CGAffineTransform(translationX: 0, y: -keyboardRectangle.height + self.view.safeAreaInsets.bottom)
+                }
+            )
+        }
+    }
     
-    
+    @objc func keyboardDown() {
+        self.userInputView.transform = .identity
+    }
     
 }
 
