6c8a5b34b9f923d3b4ad119f125b655ae1d7d94a
yeoni <ghj0308@daum.net>
2023-12-17 18:41:22 +0900

    edit: Layout 손봄..

--- Mobbie/Mobbie/Scene/Detail/CommentInputView.swift
+++ Mobbie/Mobbie/Scene/Detail/CommentInputView.swift
@@ -10,31 +10,39 @@ import SnapKit
 
 final class CommentInputView: UIView {
     
-    private let userLabel = {
+    let userLabel = {
         let label = UILabel()
+        label.textColor = .white
+        label.text = "유저닉네임이보여용"
         label.font = Design.Font.preSemiBold.midFont
         return label
     }()
     
+    let capsuleView = UIView()
+    
     let textView = {
         let view = UITextView()
-        view.backgroundColor = .gray
-        view.layer.cornerRadius = 12
+        view.backgroundColor = .clear
         view.font = Design.Font.preRegular.getFonts(size: 15)
         view.textColor = .white
+        view.contentInset = UIEdgeInsets(top: 1, left: 2, bottom: 2, right: 2)
         return view
     }()  
     
-    private let placeholderLabel = {
+    let placeholderLabel = {
         let label = UILabel()
         label.text = "답글 게시하기"
-        label.textColor = .gray.withAlphaComponent(0.6)
+        label.textColor = .lightGray
         label.font = Design.Font.preRegular.getFonts(size: 15)
         return label
     }()
     
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureHierarchy()
+        setConstraints()
+        configureView()
     }
     
     required init?(coder: NSCoder) {
@@ -44,34 +52,46 @@ final class CommentInputView: UIView {
     
     func configureHierarchy() {
         
+        capsuleView.backgroundColor = .darkGray.withAlphaComponent(0.8)
+        capsuleView.layer.cornerRadius = 12
+        
         [
             userLabel,
+            capsuleView,
             placeholderLabel,
             textView
         ]
             .forEach { self.addSubview($0) }
+        
     }
     
     func setConstraints() {
+        
         userLabel.snp.makeConstraints { make in
-            make.top.equalTo(self.safeAreaInsets).inset(16)
-            make.leading.equalToSuperview().inset(20)
-            make.height.equalTo(30)
+            make.top.equalToSuperview()
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(0)
         }
         
         placeholderLabel.snp.makeConstraints { make in
-            make.top.leading.equalTo(textView).inset(4)
+            make.top.equalTo(textView).inset(8)
+            make.leading.equalTo(textView).inset(8)
+        }
+        
+        capsuleView.snp.makeConstraints { make in
+            make.size.equalTo(textView).multipliedBy(1.05)
+            make.center.equalTo(textView)
         }
         
         textView.snp.makeConstraints { make in
             make.top.equalTo(userLabel.snp.bottom).offset(4)
             make.horizontalEdges.equalToSuperview().inset(20)
-            make.bottom.equalTo(self.safeAreaLayoutGuide).inset(4)
+            make.bottom.equalToSuperview().inset(12)
         }
     }
     
     func configureView() {
-        userLabel.text = "안뇽하세요?"
-        textView.text = "hellopeople"
+        userLabel.text = UserDefaultsHelper.shared.nickname
+        userLabel.isHidden = true
     }
 }
