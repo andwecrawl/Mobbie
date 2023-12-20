867ddee8f7582dccb12d69cc8883c3b8d53ff7bb
yeoni <ghj0308@daum.net>
2023-12-21 02:52:01 +0900

    feat: ProfileViewController

---
+++ Mobbie/Mobbie/Scene/Profile/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Mobbie
+//
+//  Created by yeoni on 12/21/23.
+//
+
+import UIKit
+
+class ProfileViewController: BaseViewController {
+    
+    let nicknameView = NicknameView()
+    
+    
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+    override func configureView() {
+        
+    }
+    
+    
+    
+}
