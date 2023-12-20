99699b37888dc2cd65d2b4a83ed874d4e810231a
yeoni <ghj0308@daum.net>
2023-12-21 02:14:41 +0900

    feat: KingfisherHelper

---
+++ Mobbie/Mobbie/Network/KingfisherHelper.swift
@@ -0,0 +1,46 @@
+//
+//  KingFisherManager.swift
+//  Mobbie
+//
+//  Created by yeoni on 12/20/23.
+//
+
+import UIKit
+import Kingfisher
+
+class KingfisherHelper {
+    
+    static let shared = KingfisherHelper()
+    
+    private init() { }
+    
+    
+    func fetchImage(imageURL: String, completionHandler: @escaping (_ image: UIImage, _ size: CGSize) -> Void, errorHandler: @escaping (Error) -> Void) {
+        
+        let modifier = AnyModifier { request in
+            var request = request
+            request.setValue(UserDefaultsHelper.shared.accessToken, forHTTPHeaderField: "Authorization")
+            request.setValue(APIKeyURL.APIKey.rawValue, forHTTPHeaderField: "SesacKey")
+            return request
+        }
+        
+        
+        if let url = URL(string: APIKeyURL.baseURL.rawValue + imageURL) {
+            KingfisherManager.shared.retrieveImage(with: url, options: [
+                .requestModifier(modifier)
+            ]) { response in
+                switch response {
+                    
+                case .success(let result):
+                    let image = result.image
+                    let size = result.image.size
+                    completionHandler(image, size)
+                    
+                case .failure(let error):
+                    print(error)
+                    errorHandler(error)
+                }
+            }
+        }
+    }
+}
