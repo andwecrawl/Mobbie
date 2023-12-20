cf29ef44aba6581e6ead29226696e49a4d5b2c7e
yeoni <ghj0308@daum.net>
2023-12-21 03:13:24 +0900

    feat: ProfileViewController UI

--- Mobbie/Mobbie/Scene/Profile/ProfileViewController.swift
+++ Mobbie/Mobbie/Scene/Profile/ProfileViewController.swift
@@ -9,22 +9,105 @@ import UIKit
 
 class ProfileViewController: BaseViewController {
     
-    let nicknameView = NicknameView()
+    private let nicknameView = NicknameView()
+    
+    private let segmentedControl = {
+        let segmentedControl = UnderlineSegmentedControl(items: ["내가 작성한 글", "미디어"])
+        return segmentedControl
+    }()
     
     
+    private let myPostsVC = {
+        let vc = FeedViewController()
+        vc.feedType = .profileFeed
+        return vc
+    }()
+    
+    private let mediaVC = MediaViewController() // 추후 수정
+    
+    private lazy var pageViewController: UIPageViewController = {
+        let vc = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        vc.setViewControllers([self.dataViewControllers[0]], direction: .forward, animated: true)
+        vc.delegate = self
+        vc.dataSource = self
+        vc.view.translatesAutoresizingMaskIntoConstraints = false
+        return vc
+    }()
+    
+    lazy var dataViewControllers: [UIViewController] = [myPostsVC, mediaVC]
+    
+    var currentPage: Int = 0 {
+        didSet {
+            let direction: UIPageViewController.NavigationDirection = oldValue <= self.currentPage ? .forward : .reverse
+            self.pageViewController.setViewControllers(
+                [dataViewControllers[self.currentPage]],
+                direction: direction,
+                animated: true,
+                completion: nil
+            )
+        }
+    }
     
     override func configureHierarchy() {
-        
+        super.configureHierarchy()
+        self.view.addSubview(self.segmentedControl)
+        self.view.addSubview(self.pageViewController.view)
     }
     
     override func setConstraints() {
+        segmentedControl.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(50)
+        }
         
+        pageViewController.view.snp.makeConstraints { make in
+            make.bottom.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(segmentedControl.snp.bottom).offset(6)
+        }
     }
     
     override func configureView() {
-        
+        self.segmentedControl.addTarget(self, action: #selector(changeValue(control:)), for: .valueChanged)
+        self.segmentedControl.selectedSegmentIndex = 0
+        self.changeValue(control: self.segmentedControl)
     }
     
     
-    
+    @objc private func changeValue(control: UISegmentedControl) {
+        self.currentPage = control.selectedSegmentIndex
+    }
 }
+
+
+extension ProfileViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerBefore viewController: UIViewController
+    ) -> UIViewController? {
+        guard
+            let index = self.dataViewControllers.firstIndex(of: viewController),
+            index - 1 >= 0
+        else { return nil }
+        return self.dataViewControllers[index - 1]
+    }
+    
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerAfter viewController: UIViewController
+    ) -> UIViewController? {
+        guard let index = self.dataViewControllers.firstIndex(of: viewController),
+            index + 1 < self.dataViewControllers.count
+        else { return nil }
+        return self.dataViewControllers[index + 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        guard let viewController = pageViewController.viewControllers?[0],
+            let index = self.dataViewControllers.firstIndex(of: viewController)
+        else { return }
+        self.currentPage = index
+        self.segmentedControl.selectedSegmentIndex = index
+    }
+}
+
