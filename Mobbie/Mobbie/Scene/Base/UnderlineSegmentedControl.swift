2f7e58f597d5e8c0cf1cfb2ea4eb249b8347ff2a
yeoni <ghj0308@daum.net>
2023-12-21 03:12:13 +0900

    feat: UnderlineSegmentedControl

---
+++ Mobbie/Mobbie/Scene/Base/UnderlineSegmentedControl.swift
@@ -0,0 +1,68 @@
+//
+//  UnderlineSegmentedControl.swift
+//  Mobbie
+//
+//  Created by yeoni on 12/21/23.
+//
+
+import UIKit
+
+final class UnderlineSegmentedControl: UISegmentedControl {
+    private lazy var underlineView: UIView = {
+        let width = self.bounds.size.width / CGFloat(self.numberOfSegments)
+        let height = 3.0
+        let xPosition = CGFloat(self.selectedSegmentIndex * Int(width))
+        let yPosition = self.bounds.size.height - 1.0
+        let frame = CGRect(x: xPosition, y: yPosition, width: width, height: height)
+        let view = UIView(frame: frame)
+        view.backgroundColor = Design.Color.tintColor
+        self.addSubview(view)
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.removeBackgroundAndDivider()
+        self.configureText()
+    }
+    override init(items: [Any]?) {
+        super.init(items: items)
+        self.removeBackgroundAndDivider()
+        self.configureText()
+    }
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func removeBackgroundAndDivider() {
+        let image = UIImage()
+        self.setBackgroundImage(image, for: .normal, barMetrics: .default)
+        self.setBackgroundImage(image, for: .selected, barMetrics: .default)
+        self.setBackgroundImage(image, for: .highlighted, barMetrics: .default)
+        
+        self.setDividerImage(image, forLeftSegmentState: .selected, rightSegmentState: .normal, barMetrics: .default)
+    }
+    
+    private func configureText() {
+        self.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.gray], for: .normal)
+        self.setTitleTextAttributes(
+            [
+                NSAttributedString.Key.foregroundColor: Design.Color.tintColor,
+                .font: Design.Font.scdreamBold.largeFont
+            ],
+            for: .selected
+        )
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let underlineFinalXPosition = (self.bounds.width / CGFloat(self.numberOfSegments)) * CGFloat(self.selectedSegmentIndex)
+        UIView.animate(
+            withDuration: 0.1,
+            animations: {
+                self.underlineView.frame.origin.x = underlineFinalXPosition
+            }
+        )
+    }
+}
