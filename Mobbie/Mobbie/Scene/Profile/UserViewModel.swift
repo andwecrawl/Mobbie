99d5ccbafbd63b4277ff267715461ce759dddca6
yeoni <ghj0308@daum.net>
2023-12-26 23:56:36 +0900

    feat: 기능 Update
    API 통신 및 output으로 전환

--- Mobbie/Mobbie/Scene/Profile/UserViewModel.swift
+++ Mobbie/Mobbie/Scene/Profile/UserViewModel.swift
@@ -11,29 +11,87 @@ import RxCocoa
 
 class UserViewModel: ViewModel {
     
-    var cellType: BehaviorRelay<ProfileCellType> = BehaviorRelay(value: .feed)
+    var posts: [Post] = []
+    var cursor = PublishSubject<String>()
+    
+    var cellType: BehaviorRelay<ProfileCellType> = BehaviorRelay(value: .media)
     
     var disposeBag = DisposeBag()
     
     
     struct Input {
-        
     }
     
     struct Output {
-        
+        let cellType: BehaviorRelay<ProfileCellType>
+        let nextCursor: BehaviorSubject<String>
+        let errorMessage: BehaviorSubject<String>
     }
     
     func transform(input: Input) -> Output? {
         
-        cellType
-            .throttle(.seconds(1), scheduler: MainScheduler.instance)
-            .bind(with: self) { owner, cellType in
-                
+        let nextCursor = BehaviorSubject(value: "")
+        let errorMessage = BehaviorSubject(value: "")
+        let data = PublishSubject<Result<PostResponse, Error>>()
+        
+        data
+            .subscribe(with: self) { owner, response in
+                switch response {
+                case .success(let result):
+                    nextCursor.onNext(result.nextCursor)
+                    
+                    if owner.cellType.value == .media {
+                        let picPosts = result.data.filter({ $0.image.count > 0 })
+                        owner.posts.append(contentsOf: picPosts)
+                        
+                    } else {
+                        owner.posts.append(contentsOf: result.data)
+                    }
+                case .failure(let error):
+                    errorMessage.onNext(error.localizedDescription)
+                }
             }
             .disposed(by: disposeBag)
         
-        return Output()
+        
+        cursor
+            .flatMapLatest({ [weak self] value in
+                
+                switch self?.cellType.value {
+                case .media:
+                    return MoyaAPIManager.shared.fetchInSignProgress(.fetchMyPosts(userID: UserDefaultsHelper.shared.userID), type: PostResponse.self)
+                case .liked:
+                    return MoyaAPIManager.shared.fetchInSignProgress(.fetchPostUserLiked, type: PostResponse.self)
+                default:
+                    return MoyaAPIManager.shared.fetchInSignProgress(.fetchMyPosts(userID: UserDefaultsHelper.shared.userID), type: PostResponse.self)
+                }
+            })
+            .subscribe(with: self) { owner, response in
+                switch response {
+                case .success(let result):
+                    print(result)
+                    data.onNext(.success(result))
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    data.onNext(.failure(error))
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        
+        cellType
+            .subscribe(with: self) { owner, _ in
+                owner.cursor.onNext("")
+                owner.posts = []
+            }
+            .disposed(by: disposeBag)
+        
+        
+        return Output(
+            cellType: cellType,
+            nextCursor: nextCursor,
+            errorMessage: errorMessage
+        )
     }
     
     
