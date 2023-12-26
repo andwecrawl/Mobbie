3cbedf3091b0814d276d008c1c2c365bedb68eec
yeoni <ghj0308@daum.net>
2023-12-26 23:56:01 +0900

    feat: UI Setting

--- Mobbie/Mobbie/Scene/Profile/CollectionViewCell/NicknameCell.swift
+++ Mobbie/Mobbie/Scene/Profile/CollectionViewCell/NicknameCell.swift
@@ -16,12 +16,28 @@ class NicknameCell: BaseCollectionViewCell {
         var config = UIButton.Configuration.bordered()
         config.cornerStyle = .capsule
         config.baseBackgroundColor = .clear
-        var container = AttributeContainer()
-        container.foregroundColor = UIColor.white
-        container.font = Design.Font.preMedium.smallFont
-        config.attributedTitle = AttributedString("프로필 수정", attributes: container)
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 12, weight: .medium)
+        let image = UIImage(systemName: "gear", withConfiguration: imageConfig)
+        config.image = image
         view.configuration = config
-        view.layer.cornerRadius = 15
+        view.tintColor = UIColor.gray
+        view.layer.cornerRadius = 12
+        view.layer.borderColor = UIColor.gray.withAlphaComponent(0.6).cgColor
+        view.layer.borderWidth = 1
+        return view
+    }()
+    
+    let alertButton = {
+        let view = UIButton()
+        var config = UIButton.Configuration.bordered()
+        config.cornerStyle = .capsule
+        config.baseBackgroundColor = .clear
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 12, weight: .medium)
+        let image = UIImage(systemName: "bell.fill", withConfiguration: imageConfig)
+        config.image = image
+        view.configuration = config
+        view.tintColor = UIColor.gray
+        view.layer.cornerRadius = 12
         view.layer.borderColor = UIColor.gray.withAlphaComponent(0.6).cgColor
         view.layer.borderWidth = 1
         return view
@@ -30,6 +46,7 @@ class NicknameCell: BaseCollectionViewCell {
     override func configureHierarchy() {
         contentView.addSubview(nicknameView)
         contentView.addSubview(settingButton)
+        contentView.addSubview(alertButton)
     }
     
     override func setConstraints() {
@@ -38,10 +55,15 @@ class NicknameCell: BaseCollectionViewCell {
             make.horizontalEdges.bottom.equalToSuperview()
         }
         
-        settingButton.snp.makeConstraints { make in
+        alertButton.snp.makeConstraints { make in
             make.top.trailing.equalToSuperview().inset(12)
-            make.width.equalTo(90)
-            make.height.equalTo(30)
+            make.size.equalTo(40)
+        }
+        
+        settingButton.snp.makeConstraints { make in
+            make.top.equalTo(alertButton)
+            make.trailing.equalTo(alertButton).inset(46)
+            make.size.equalTo(40)
         }
     }
     
